0812174007d8dcef4dcef2cbe10d27d17aa3eab3
Soft-Atom <tabproant@gmail.com>
2023-07-09 10:35:57 +0300

    2

--- src/7-swap/generics.ts
+++ src/7-swap/generics.ts
@@ -1,4 +1,7 @@
 {
+	// не совсем понял смысл задания.
+	// где тут используются generics
+	
 	const obj: Record<string, unknown> = {
 		a: 1,
 		b: 2
@@ -8,6 +11,26 @@
 		const res = {};
 		for (let key in obj) {
 			res[String(obj[key])] = key;
+		}
+		return res;
+	}
+
+	const res = swapKeysAndValues(obj);
+	console.log(res);
+}
+
+{
+	// может так
+	
+	const obj: Record<string, number> = {
+		a: 1,
+		b: 2
+	}
+
+	function swapKeysAndValues(obj: Record<string, number>):Record<number, string> {
+		const res = {};
+		for (let key in obj) {
+			res[obj[key]] = key;
 		}
 		return res;
 	}
