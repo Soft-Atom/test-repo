20f94d7d9745952da9a6b1be09f401d2cd82b950
Soft-Atom <tabproant@gmail.com>
2023-07-17 09:56:38 +0300

    Исправления

--- src/7-swap/generics.ts
+++ src/7-swap/generics.ts
@@ -1,25 +1,25 @@
-{
-	// не совсем понял смысл задания.
-	// где тут используются generics
+// {
+// 	// не совсем понял смысл задания.
+// 	// где тут используются generics
 	
-	const obj: Record<string, unknown> = {
-		a: 1,
-		b: 2
-	}
+// 	const obj: Record<string, unknown> = {
+// 		a: 1,
+// 		b: 2
+// 	}
 
-	function swapKeysAndValues(obj: Record<string, unknown>): Record<string, string> {
-		const res = {};
-		for (let key in obj) {
-			res[String(obj[key])] = key;
-		}
-		return res;
-	}
+// 	function swapKeysAndValues(obj: Record<string, unknown>): Record<string, string> {
+// 		const res = {};
+// 		for (let key in obj) {
+// 			res[String(obj[key])] = key;
+// 		}
+// 		return res;
+// 	}
 
-	const res = swapKeysAndValues(obj);
-	console.log(res);
-}
+// 	const res = swapKeysAndValues(obj);
+// 	console.log(res);
+// }
 
-{
+{// скобки оставил, иначе повторное использование переменной res
 	// может так
 	
 	const obj: Record<string, number> = {
@@ -28,10 +28,8 @@
 	}
 
 	function swapKeysAndValues(obj: Record<string, number>):Record<number, string> {
-		const res = {};
-		for (let key in obj) {
-			res[obj[key]] = key;
-		}
+		const res: Record<number, string> = {};
+		Object.entries(obj).forEach(([key, val]) => res[val] = key)
 		return res;
 	}
 
--- src/8-pick/pick.ts
+++ src/8-pick/pick.ts
@@ -1,22 +1,22 @@
-{
+{ // скобки оставил, иначе конфликт на res
 	const user = {
 		name:'Vasya',
 		age: 8,
 		skills: ['typescript', 'javascript']
 	}
 
-	type TKey = string | number | symbol;
+	type KeyT = string | number | symbol;
 	type TObj = Record<TKey, any>;
 
 	function pickObjectKeys<T extends TObj>(obj: T, keys: (keyof T)[]): T {
 		//после вот этого вот в памяти остается объект obj ?
 		//или он будет удален сборщиком мусора ? 
-		return keys.reduce(
-			(acc:T, el) => {
+		return keys.reduce<T>(
+			(acc:T, el): T => {
 				acc[el] = obj[el];
 				return acc;
 			},
-			<T>{}
+			{} as T
 		);
 	}
 	const res = pickObjectKeys(user,['age','name']);
